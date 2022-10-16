41de9699a7a35b2145566ea57c70cf8b1d4b0cff
Acheampong Stephen <acheampongstephen392024@gmail.com>
2022-10-16 05:57:11 -0700

    params update

--- src/get_data.py
+++ src/get_data.py
@@ -1,9 +1,21 @@
 import os
 import yaml
+import argparse
 import pandas as pd
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    print(config)
 
 
 
 if __name__ == "__main__":
-    
+    arg = argparse.ArgumentParser()
+    arg.add_argument("--config", default="params.yaml")
+    parsed_args = arg.parse_args()
+    data = get_data(config_path = parsed_args.config)
